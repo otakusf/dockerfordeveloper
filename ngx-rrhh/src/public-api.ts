e50801710f14159bf39baacc1fe7cc4898f8dbdc
otakusf <santacruz.freddy@gmail.com>
2026-04-16 07:45:52 -0500

    quitar archivos

--- ngx-rrhh/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview NGX-RRHH - Angular 21 CRUD Application
- * @description HR Management System with PrimeNG components
- */
-
-export * from './app/app.component';
-export * from './app/app.config';
-export * from './app/app.routes';
